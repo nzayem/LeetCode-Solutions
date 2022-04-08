977db45a535b0496f99ee9b42b931ea7ddb35868
A. Nzayem <nzayem@gmail.com>
2022-04-08 14:43:40 +0300

    Add files via upload

---
+++ Training/Easy-1512-Number-of-Good-Pairs.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j) {
+                    counter++;
+                }
+            }
+        }
+       
+        return counter;        
+        
+    }
+}
